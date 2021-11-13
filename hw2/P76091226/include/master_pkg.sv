eb5e00d8fc950948c415d5facc3f0816411c5d8a
WeiCheng14159 <michael1996623@gmail.com>
2021-11-13 22:03:44 +0800

    fix-Try new master

--- hw2/P76091226/include/master_pkg.sv
+++ hw2/P76091226/include/master_pkg.sv
@@ -7,12 +7,16 @@ package master_pkg;
   localparam VERSION = "v1.0";
   localparam AUTHOR = "Wei Cheng";
 
-  localparam LOCK_BIT = 0, UNLOCK_BIT = 1;
+  localparam IDLE_BIT = 0, AR_BIT = 1, R_BIT = 2, AW_BIT = 3, W_BIT = 4, B_BIT = 5;
 
-  typedef enum logic {
-    LOCK   = 1'b1,
-    UNLOCK = 1'b0
-  } lock_state_t;
+  typedef enum logic [6:0] {
+    IDLE = 1 << IDLE_BIT,
+    AR = 1 << AR_BIT,
+    R = 1 << R_BIT,
+    AW = 1 << AW_BIT,
+    W = 1 << W_BIT,
+    B = 1 << B_BIT
+  } master_state_t;
 
 endpackage : master_pkg
 
--- hw2/P76091226/src/AXI/master.sv
+++ hw2/P76091226/src/AXI/master.sv
@@ -50,108 +50,102 @@ module master
     output logic stall
 );
 
-  logic [`AXI_ID_BITS-1:0] master_ID;
-  logic ar_fin, r_fin, aw_fin, w_fin, b_fin;
-  logic read_stall, write_stall;
-  logic r;
-  logic [`AXI_DATA_BITS-1:0] RDATA_r;
-  lock_state_t
-      ar_lock, aw_lock, w_lock, rx_wait, wx_wait, rready_lock, bready_lock;
 
-  assign master_ID = `AXI_ID_BITS'b0;
-  // ARx
-  assign ARID_M = master_ID;
-  assign ARLEN_M = `AXI_LEN_BITS'b0;
-  assign ARSIZE_M = `AXI_SIZE_BITS'b10;
-  assign ARBURST_M = 2'b0;
-  assign ARVALID_M = (rx_wait) ? 1'b0 : (ar_lock | read) & r;
-  assign ARADDR_M = addr;
-  // Rx
-  assign RREADY_M = (  /*aw_fin |*/ rready_lock);
-  // AWx
-  assign AWID_M = master_ID;
-  assign AWVALID_M = (wx_wait) ? 1'b0 : (aw_lock | write);
-  assign AWLEN_M = `AXI_LEN_BITS'b0;
-  assign AWSIZE_M = `AXI_SIZE_BITS'b10;
-  assign AWBURST_M = 2'b0;
-  assign AWADDR_M = addr;
-  // Wx
-  assign WVALID_M =  /* aw_fin | */ w_lock;
-  assign WDATA_M = data_in;
-  assign WSTRB_M = w_type;
-  assign WLAST_M = 1'b1;
-  // Bx
-  assign BREADY_M = bready_lock /*| w_fin*/;
-  // *_fin
-  assign ar_fin = ARREADY_M & ARVALID_M;
-  assign r_fin = RREADY_M & RVALID_M;
-  assign aw_fin = AWREADY_M & AWVALID_M;
-  assign w_fin = WREADY_M & WVALID_M;
-  assign b_fin = BREADY_M & BVALID_M;
-  // CPU interface
-  assign data_out = (r_fin) ? RDATA_M : RDATA_r;
-  assign write_stall = write & ~w_fin;
-  assign read_stall = read & ~r_fin;
-  assign stall = read_stall | write_stall;
+  logic [`AXI_DATA_BITS-1:0] RDATA_M_r;
+  master_state_t m_curr_state, m_next_state;
+  logic ARx_hs_done, Rx_hs_done, AW_hs_done, Wx_hs_done, Bx_hs_done;
 
+  assign AW_hs_done = AWVALID_M & AWREADY_M;
+  assign Wx_hs_done = WVALID_M & WREADY_M;
+  assign Bx_hs_done = BVALID_M & BREADY_M;
+  assign ARx_hs_done = ARVALID_M & ARREADY_M;
+  assign Rx_hs_done = RVALID_M & RREADY_M;
+
+  assign data_out = Rx_hs_done ? RDATA_M : RDATA_M_r;
   always_ff @(posedge clk or negedge rstn) begin
-    if (~rstn) r <= 1'b0;
-    else r <= 1'b1;
+    if (~rstn) RDATA_M_r <= `AXI_DATA_BITS'b0;
+    else RDATA_M_r <= (Rx_hs_done) ? RDATA_M : RDATA_M_r;
   end
 
-  // ar_lock
-  always_ff @(posedge clk or negedge rstn) begin
-    if (~rstn) ar_lock <= UNLOCK;
-    else
-      ar_lock <= (ar_lock == LOCK && ARREADY_M) ? UNLOCK : (ARVALID_M && ~ARREADY_M) ? LOCK : ar_lock;
-  end
+  always_ff @(posedge clk, negedge rstn) begin
+    if (~rstn) begin
+      m_curr_state <= IDLE;
+    end else begin
+      m_curr_state <= m_next_state;
+    end
+  end  // State
 
-  // aw_lock
-  always_ff @(posedge clk or negedge rstn) begin
-    if (~rstn) aw_lock <= UNLOCK;
-    else
-      aw_lock <= (aw_lock == LOCK && AWREADY_M) ? UNLOCK : (AWVALID_M && ~AWREADY_M) ? LOCK : aw_lock;
-  end
+  always_comb begin
+    m_next_state = IDLE;
+    case (m_curr_state)
+      IDLE: m_next_state = (write) ? AW : (read) ? AR : IDLE;
+      AR: m_next_state = (ARREADY_M) ? R : AR;
+      R: m_next_state = (RREADY_M) ? (write ? AW : read ? AR : IDLE) : R;
+      AW: m_next_state = (AWREADY_M) ? W : AW;
+      W: m_next_state = (WREADY_M) ? B : W;
+      B: m_next_state = (BREADY_M) ? (write ? AW : read ? AR : IDLE) : B;
+      default: ;
+    endcase
+  end  // Next state (C)
 
-  // w_lock
-  always_ff @(posedge clk or negedge rstn) begin
-    if (~rstn) w_lock <= UNLOCK;
-    else
-      w_lock <= (w_lock == LOCK && WREADY_M) ? UNLOCK : (WVALID_M && ~WREADY_M) ? LOCK : w_lock;
-  end
+  always_comb begin
+    // AWx
+    AWID_M = `AXI_IDS_BITS'b0;
+    AWADDR_M = addr;
+    AWLEN_M = `AXI_LEN_BITS'b0;
+    AWSIZE_M = `AXI_SIZE_BITS'b0;
+    AWBURST_M = `AXI_BURST_INC;
+    AWVALID_M = 1'b0;
+    // Wx
+    WDATA_M = data_in;
+    WSTRB_M = w_type;
+    WLAST_M = 1'b1;
+    WVALID_M = 1'b0;
+    // Bx
+    BREADY_M = 1'b0;
+    // ARx
+    ARID_M = `AXI_IDS_BITS'b0;
+    ARADDR_M = addr;
+    ARLEN_M = `AXI_LEN_BITS'b0;
+    ARSIZE_M = `AXI_SIZE_BITS'b0;
+    ARBURST_M = `AXI_BURST_INC;
+    ARVALID_M = 1'b0;
+    // Rx
+    RREADY_M = 1'b0;
 
-  // bready_lock
-  always_ff @(posedge clk or negedge rstn) begin
-    if (~rstn) bready_lock <= UNLOCK;
-    else
-      bready_lock <= (bready_lock == LOCK && BVALID_M) ? UNLOCK : (w_fin) ? LOCK : bready_lock;
-  end
+    case (m_curr_state)
+      IDLE: begin
 
-  // rready_lock
-  always_ff @(posedge clk or negedge rstn) begin
-    if (~rstn) rready_lock <= UNLOCK;
-    else
-      rready_lock <= (rready_lock == LOCK && RVALID_M) ? UNLOCK : (ar_fin) ? LOCK : rready_lock;
-  end
-
-  // rx_wait
-  always_ff @(posedge clk or negedge rstn) begin
-    if (~rstn) rx_wait <= UNLOCK;
-    else
-      rx_wait <= (rx_wait == LOCK && r_fin) ? UNLOCK : (ar_fin & ~(RREADY_M && RVALID_M)) ? LOCK : rx_wait;
-  end
-
-  // wx_wait
-  always_ff @(posedge clk or negedge rstn) begin
-    if (~rstn) wx_wait <= UNLOCK;
-    else
-      wx_wait <= (wx_wait == LOCK && b_fin) ? UNLOCK : (aw_fin & ~b_fin) ? LOCK : wx_wait;
-  end
-
-  // RDATA_r
-  always @(posedge clk or negedge rstn) begin
-    if (~rstn) RDATA_r <= `AXI_DATA_BITS'b0;
-    else RDATA_r <= (r_fin) ? RDATA_M : RDATA_r;
+      end
+      AR: begin
+        // ARx
+        ARBURST_M = `AXI_BURST_INC;
+        ARVALID_M = 1'b1;
+        // Rx
+        RREADY_M  = 1'b1;
+      end
+      R: begin
+        // Rx
+        RREADY_M = 1'b1;
+      end
+      AW: begin
+        // AWx
+        AWVALID_M = 1'b1;
+        // Wx
+        WVALID_M  = 1'b1;
+      end
+      W: begin
+        // Wx
+        WVALID_M = 1'b1;
+        // Bx
+        BREADY_M = 1'b1;
+      end
+      B: begin
+        // Bx
+        BREADY_M = 1'b1;
+      end
+      default: ;
+    endcase
   end
 
 endmodule
