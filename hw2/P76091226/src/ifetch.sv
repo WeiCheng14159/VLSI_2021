e185e8ef40c8de19a04109deb345a3e76464f3f5
WeiCheng14159 <michael1996623@gmail.com>
2021-11-17 12:49:16 +0800

    fix-New ifetch unit that deal with stall request and branch
    - Must deal with stall request from I-MEM, D-MEM, and ID stage
    - Branch instruction during stall should also be taken into
    consideration

--- hw2/P76091226/src/ifetch.sv
+++ hw2/P76091226/src/ifetch.sv
@@ -7,6 +7,7 @@ module ifetch (
     input logic                  flush,
     input logic [       `RegBus] branch_target_addr_i,
     input logic                  branch_taken_i,
+    input logic                  is_id_branch_inst,
     input logic [       `RegBus] new_pc_i,
     input logic [      `InstBus] inst_i,
     input logic [       `RegBus] id_pc_i,
@@ -17,46 +18,74 @@ module ifetch (
     output logic [`InstBus] inst_o
 );
 
-  logic [       `RegBus]  fetch_pc, fetch_pc_n1;
-  logic [`STAGE_NUM-1:0 ] stall_prev;
-  logic [       `RegBus]  next_pc;
+  logic [       `RegBus] fetch_pc, fetch_pc_next, next_pc;
+  logic [       `RegBus] branch_target_addr_r, branch_not_taken_addr_r;
+  logic                  if_stall, branch_taken;
 
-  assign inst_addr_o = (branch_taken_i == `BranchTaken) ? branch_target_addr_i : fetch_pc;
+  assign if_stall = stall[`IF_STAGE] == `Stop;
+  assign branch_taken = (branch_taken_i == `BranchTaken);
+  assign inst_addr_o = fetch_pc;
   assign next_pc = fetch_pc + 4;
 
   // inst_read_o
-  assign inst_read_o = (stall[`IF_STAGE] == `Stop | branch_taken_i == `BranchTaken) ? `ReadDisable : `ReadEnable;
-
-  // fetch_pc_n1
-  always_ff @(posedge clk, negedge rstn) begin
-    if (~rstn) begin
-      fetch_pc_n1 <= `StartAddr;
-    end else begin
-      fetch_pc_n1 <= (stall[`IF_STAGE] == `Stop) ? fetch_pc_n1 : inst_addr_o;
-    end
-  end
+  assign inst_read_o = (if_stall | branch_taken | is_id_branch_inst) ? `ReadDisable : `ReadEnable;
 
   // if_pc_o
   always_ff @(posedge clk, negedge rstn) begin
     if (~rstn) begin
       if_pc_o <= `StartAddr;
+      fetch_pc_next <= `StartAddr;
     end else begin
-      if_pc_o <= /*(stall[`IF_STAGE] == `Stop) ? if_pc_o : */fetch_pc_n1;
+      if_pc_o <= (if_stall) ? if_pc_o : fetch_pc_next;
+      fetch_pc_next <= (if_stall) ? fetch_pc_next : fetch_pc;
     end
   end
 
   // fetch_pc
   always_ff @(posedge clk, negedge rstn) begin
-    if (~rstn) begin
+    if(~rstn) begin
       fetch_pc <= `StartAddr;
     end else begin
-      fetch_pc <= (branch_taken_i == `BranchTaken) ? branch_target_addr_i :
-                  (stall[`IF_STAGE] == `Stop) ? fetch_pc :
-                  next_pc;
+      if(if_stall) begin
+        if(is_id_branch_inst & branch_taken) begin
+          fetch_pc <= branch_target_addr_i;
+        end else if(is_id_branch_inst & ~branch_taken) begin
+          fetch_pc <= fetch_pc;
+        end else begin
+          fetch_pc <= fetch_pc;
+        end
+      end else begin
+        // no if_stall
+        if(is_id_branch_inst & branch_taken) begin
+          fetch_pc <= branch_target_addr_r;
+        end else if(is_id_branch_inst & ~branch_taken) begin
+          fetch_pc <= branch_not_taken_addr_r;
+        end else begin
+          fetch_pc <= next_pc;
+        end
+      end
+
+    end
+  end
+
+  // branch_target_addr_r
+  always_ff @(posedge clk, negedge rstn) begin
+    if(~rstn) begin
+      branch_target_addr_r <= `StartAddr;
+      branch_not_taken_addr_r <= `StartAddr;
+    end else begin
+      branch_target_addr_r <= (is_id_branch_inst) ? branch_target_addr_i : branch_target_addr_r;
+      branch_not_taken_addr_r <= (~is_id_branch_inst) ? next_pc : branch_not_taken_addr_r;
     end
   end
 
   // inst_o
-  assign inst_o = inst_i;
+  always_ff @(posedge clk, negedge rstn) begin
+    if(~rstn) begin
+      inst_o <= `StartAddr;
+    end else begin
+      inst_o <= (if_stall) ? inst_o : inst_i;
+    end
+  end
 
 endmodule
