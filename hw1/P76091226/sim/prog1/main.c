e5d5bc3166f1571cee9ae35a2f229a1f923ee739
WeiCheng14159 <michael1996623@gmail.com>
2021-11-20 21:35:00 +0800

    fix-Bug in program 1: insertion sort

--- hw1/P76091226/sim/prog1/main.c
+++ hw1/P76091226/sim/prog1/main.c
@@ -1,11 +1,7 @@
-extern int _test_start;
-extern int array_size;
-extern int array_addr;
-
 void swap(int *a, int *b);
 void selectionSort(int arr[], int n, int test[]){
   int i, j, min_idx;
-  for(i = 0; i < n-1 ; i++){
+  for(i = 0; i < n ; i++){
     min_idx = i;
     for(j = i+1 ; j < n ; j++)
       if(arr[j] < arr[min_idx])
@@ -13,10 +9,12 @@ void selectionSort(int arr[], int n, int test[]){
     swap(&arr[min_idx], &arr[i]);
     test[i] = arr[i];
   }
-  test[n-1] = arr[n-1];
 }
 
 int main(void){
+  extern int _test_start;
+  extern int array_size;
+  extern int array_addr;
   selectionSort(&array_addr, array_size, &_test_start);
   return 0;
 }
