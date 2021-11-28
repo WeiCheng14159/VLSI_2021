196530556330cbff321309e69b400fb06f317169
WeiCheng14159 <michael1996623@gmail.com>
2021-11-29 01:36:27 +0800

    fix-Add half word and byte support for load (WIP)

--- hw3/P76091226/src/wb.sv
+++ hw3/P76091226/src/wb.sv
@@ -7,16 +7,28 @@ module wb(
 
   output logic                [`RegBus] wdata_o
 );
+  logic                 [`RegBus] load_addr_i;
+  assign load_addr_i = from_reg_i;
 
   // wdata_o
   always_comb begin
     if (mem2reg_i == `Mem2Reg) begin
       case(func3_i) // LW, LH, LB, LBU, LHU
         `OP_LB:  wdata_o = $signed(from_mem_i[7:0]);
-        `OP_LH:  wdata_o = $signed(from_mem_i[15:0]);
+        `OP_LH:  begin
+          case(load_addr_i[1])
+            1'b1: wdata_o = $signed(from_mem_i[31:16]);
+            1'b0: wdata_o = $signed(from_mem_i[15:0]);
+          endcase
+        end
         `OP_LW:  wdata_o = from_mem_i;
         `OP_LBU: wdata_o = {24'h0, from_mem_i[7:0]};
-        `OP_LHU: wdata_o = {16'h0, from_mem_i[15:0]};
+        `OP_LHU: begin  
+          case(load_addr_i[1])
+            1'b1: wdata_o = {16'h0, from_mem_i[31:16]};
+            1'b0: wdata_o = {16'h0, from_mem_i[15:0]};
+          endcase
+        end
         default: wdata_o = from_mem_i;
       endcase
     end else begin
