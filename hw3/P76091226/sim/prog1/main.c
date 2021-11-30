93f83a71ed54c3929e55a0dea8908bf5e67de6f2
WeiCheng14159 <michael1996623@gmail.com>
2021-11-30 19:03:01 +0800

    fix-Add original prog1

--- hw3/P76091226/sim/prog1/main.c
+++ hw3/P76091226/sim/prog1/main.c
@@ -1,7 +1,26 @@
-int main(void) {
-  extern unsigned int array_size;
-  extern short array_addr;
-  extern short _test_start;
+void swap(short *a, short *b);
+void selectionSort(short arr[], int n, short test[]){
+  int i, j, min_idx;
+  for(i = 0; i < n ; i++){
+    min_idx = i;
+    for(j = i+1 ; j < n ; j++)
+      if(arr[j] < arr[min_idx])
+        min_idx = j;
+    swap(&arr[min_idx], &arr[i]);
+    test[i] = arr[i];
+  }
+}
 
+int main(void){
+  extern short _test_start;
+  extern int array_size;
+  extern short array_addr;
+  selectionSort(&array_addr, array_size, &_test_start);
   return 0;
+}
+
+void swap(short *a, short *b) {
+  short temp = *a;
+  *a = *b;
+  *b = temp;
 }
