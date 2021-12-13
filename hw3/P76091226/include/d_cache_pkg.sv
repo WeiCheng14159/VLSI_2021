1bbd1cd5efd8722490c99459a397f3bda1c6a32c
Wei Cheng <michael1996623@gmail.com>
2021-12-13 20:05:18 +0800

    feat-Finalize L1 data cache

--- hw3/P76091226/include/d_cache_pkg.sv
+++ hw3/P76091226/include/d_cache_pkg.sv
@@ -5,19 +5,20 @@ package d_cache_pkg;
   localparam VERSION = "v1.0";
   localparam AUTHOR = "Wei Cheng";
 
-  localparam VALID = 1'b1, INVALID = 1'b0;
-  localparam CACHE_HIT = 1'b1, CACHE_MISS = 1'b0;
+  localparam DA_WRITE_ENB = 1'b0, DA_WRITE_DIS = 1'b1;
+  localparam DA_READ_ENB = 1'b1, DA_READ_DIS = 1'b0;
+  localparam TA_WRITE_ENB = 1'b0, TA_WRITE_DIS = 1'b1;
+  localparam TA_READ_ENB = 1'b1, TA_READ_DIS = 1'b0;
 
   localparam IDLE_BIT = 0, CHK_BIT = 1, WHIT_BIT = 2, WMISS_BIT = 3, 
-  RMISS_BIT = 4, FIN_BIT = 5;
+  RMISS_BIT = 4;
 
-  typedef enum logic [5:0] {
+  typedef enum logic [4:0] {
     IDLE  = 1 << IDLE_BIT,
     CHK   = 1 << CHK_BIT,
     WHIT  = 1 << WHIT_BIT,
     WMISS = 1 << WMISS_BIT,
-    RMISS = 1 << RMISS_BIT,
-    FIN   = 1 << FIN_BIT
+    RMISS = 1 << RMISS_BIT
   } dcache_state_t;
 
 endpackage : d_cache_pkg
