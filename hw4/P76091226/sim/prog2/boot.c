dce1910b4410288e1cbd5e6c512a2fbadbe55d41
Wei Cheng <michael1996623@gmail.com>
2021-12-19 14:33:19 +0800

    feat-Add testing program

--- hw4/P76091226/sim/prog2/boot.c
+++ hw4/P76091226/sim/prog2/boot.c
@@ -11,5 +11,18 @@ void boot() {
   extern unsigned int __data_end;
   extern unsigned int __data_paddr_start;
 
+	int i;
+	int num;
 
+	num = (&_dram_i_end) - (&_dram_i_start);
+	for(i = 0; i <= num; i++)
+		(&_imem_start)[i] = (&_dram_i_start)[i];
+
+	num = (&__sdata_end) - (&__sdata_start);
+	for(i = 0; i <= num; i++)
+		(&__sdata_paddr_start)[i] = (&__sdata_start)[i];
+
+	num = (&__data_end) - (&__data_start);
+	for(i = 0; i <= num; i++)
+		(&__data_paddr_start)[i] = (&__data_start)[i];
 }
