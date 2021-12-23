84ebcfc97dbf58b767ec10339005ad465770a457
Wei Cheng <michael1996623@gmail.com>
2021-12-23 15:22:42 +0800

    fix-Booting program bug
    - Booting program should move instruction from DRAM to IM, and move data
    from DRAM to DM.

--- hw4/P76091226/sim/prog0/boot.c
+++ hw4/P76091226/sim/prog0/boot.c
@@ -1,28 +1,31 @@
 void boot() {
-  extern unsigned int _dram_i_start;
-  extern unsigned int _dram_i_end;
-  extern unsigned int _imem_start;
+  extern unsigned int _dram_i_start; // instruction start address in DRAM
+  extern unsigned int _dram_i_end; // instruction end address in DRAM
+  extern unsigned int _imem_start; // instruction start address in IM
 
-  extern unsigned int __sdata_start;
-  extern unsigned int __sdata_end;
-  extern unsigned int __sdata_paddr_start;
+  extern unsigned int __sdata_start; // Main_data start address in DM
+  extern unsigned int __sdata_end; // Main_data end address in DM
+  extern unsigned int __sdata_paddr_start; // Main_data start address in DRAM
 
-  extern unsigned int __data_start;
-  extern unsigned int __data_end;
-  extern unsigned int __data_paddr_start;
+  extern unsigned int __data_start; // Main_data start address in DM
+  extern unsigned int __data_end; // Main_data end address in DM
+  extern unsigned int __data_paddr_start; // Main_data start address in DRAM
 
 	int i;
 	int num;
 
+	// Move instruction from DRAM to IM
 	num = (&_dram_i_end) - (&_dram_i_start);
 	for(i = 0; i <= num; i++)
 		(&_imem_start)[i] = (&_dram_i_start)[i];
 
-	num = (&__sdata_end) - (&__sdata_start);
-	for(i = 0; i <= num; i++)
-		(&__sdata_paddr_start)[i] = (&__sdata_start)[i];
-
+	// Move data from DRAM to DM (part 1)
 	num = (&__data_end) - (&__data_start);
 	for(i = 0; i <= num; i++)
-		(&__data_paddr_start)[i] = (&__data_start)[i];
+		(&__data_start)[i] = (&__data_paddr_start)[i];
+
+	// Move data from DRAM to DM (part 2)
+	num = (&__sdata_end) - (&__sdata_start);
+	for(i = 0; i <= num; i++)
+		(&__sdata_start)[i] = (&__sdata_paddr_start)[i];
 }
