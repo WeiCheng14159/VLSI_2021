9fa5e9ba544e98c5de433a9329675671536b0bfd
Wei Cheng <michael1996623@gmail.com>
2021-12-24 16:22:00 +0800

    feat-Update sensor wrapper
    - Update sensor wrapper for one-hot encoding FSM
    - Readable variable names

---
+++ hw4/P76091226/include/sensor_wrapper_pkg.sv
@@ -0,0 +1,30 @@
+`ifndef SENSOR_WRAPPER_PKG_SV
+`define SENSOR_WRAPPER_PKG_SV
+
+package sensor_wrapper_pkg;
+  localparam VERSION = "v1.0";
+  localparam AUTHOR = "Wei Cheng";
+
+  localparam ADDR_SIZE = 6;
+  localparam DATA_SIZE = 32;
+
+  localparam EMPTY_ADDR = {ADDR_SIZE{1'b0}};
+  localparam EMPTY_DATA = {DATA_SIZE{1'b0}};
+
+  localparam SCTRL_ENB = 1'b1, SCTRL_DIS = 1'b0;
+  localparam SCTRL_CLEAR_ENB = 1'b1, SCTRL_CLEAR_DIS = 1'b0;
+
+  localparam SCTRL_ENB_ADDR = 10'h40;
+  localparam SCTRL_CLEAR_ADDR = 10'h80;
+
+  localparam IDLE_BIT = 0, READ_BIT = 1, WRITE_BIT = 2;
+
+  typedef enum logic [2:0] {
+    IDLE  = 1 << IDLE_BIT,
+    READ  = 1 << READ_BIT,
+    WRITE = 1 << WRITE_BIT
+  } sensor_wrapper_state_t;
+
+endpackage : sensor_wrapper_pkg
+
+`endif
--- hw4/P76091226/src/sensor_wrapper.sv
+++ hw4/P76091226/src/sensor_wrapper.sv
@@ -1,66 +1,59 @@
 `include "pkg_include.sv"
 `include "sensor_ctrl.sv"
-`define EN_ADDR 10'h40
-`define CLEAR_ADDR 10'h80 
 
-module sensor_wrapper (
+module sensor_wrapper
+  import sensor_wrapper_pkg::*;
+(
     input  logic                       clk,
     input  logic                       rstn,
-           AXI_slave_intf.slave        slave,
     input  logic                       sensor_ready,
     input  logic                [31:0] sensor_out,
     output logic                       sensor_en,
-    output logic                       sensor_interrupt
+    output logic                       sensor_interrupt,
+           AXI_slave_intf.slave        slave
 );
 
-  parameter STATE_IDLE = 2'b0, STATE_READ = 2'b1, STATE_WRITE = 2'b10;
-  logic sctrl_en;
-  logic sctrl_clear;
-  logic [4:0] sctrl_addr;
-  logic [1:0] state;
-  logic [1:0] nxt_state;
-  logic [`AXI_IDS_BITS-1:0] prevID;
-  logic [`AXI_LEN_BITS-1:0] prevLEN;
-  logic [`AXI_SIZE_BITS-1:0] prevSIZE;
-  logic [`AXI_ADDR_BITS-1:0] prevADDR;
-  logic [1:0] prevBURST;
-  logic ARFin;
-  logic AWFin;
-  logic RFin;
-  logic BFin;
-  logic WFin;
-  logic [9:0] sensorADDR;
+  logic sctrl_en, sctrl_clear;
+  logic [9:0] sensor_addr;
+  logic [ADDR_SIZE-1:0] sctrl_addr;
+  // AXI register
+  logic [`AXI_IDS_BITS-1:0] ID_r;
+  logic [`AXI_LEN_BITS-1:0] LEN_r;
+  logic [`AXI_SIZE_BITS-1:0] SIZE_r;
+  logic [`AXI_ADDR_BITS-1:0] ADDR_r;
+  logic [1:0] BURST_r;
+  logic lockAW, lockB, lockW;
+  logic ARx_hs_done, AWx_hs_done, Rx_hs_done, Bx_hs_done, Wx_hs_done;
   logic [`AXI_LEN_BITS-1:0] cnt;
-  logic lockAW;
-  logic lockB;
-  logic lockW;
 
-  assign ARFin = slave.ARVALID & slave.ARREADY;
-  assign AWFin = slave.AWVALID & slave.AWREADY;
-  assign RFin = slave.RVALID & slave.RREADY;
-  assign BFin = slave.BVALID & slave.BREADY;
-  assign WFin = slave.WVALID & slave.WREADY;
-  assign slave.RID = prevID;
+  sensor_wrapper_state_t curr_state, next_state;
+
+  assign ARx_hs_done = slave.ARVALID & slave.ARREADY;
+  assign AWx_hs_done = slave.AWVALID & slave.AWREADY;
+  assign Rx_hs_done  = slave.RVALID & slave.RREADY;
+  assign Bx_hs_done  = slave.BVALID & slave.BREADY;
+  assign Wx_hs_done  = slave.WVALID & slave.WREADY;
+
+  assign slave.RID   = ID_r;
   assign slave.RRESP = `AXI_RESP_OKAY;
-  assign slave.RLAST = cnt == prevLEN;
-  assign slave.BID = prevID;
+  assign slave.RLAST = cnt == LEN_r;
+  assign slave.BID   = ID_r;
   assign slave.BRESP = `AXI_RESP_OKAY;
 
-
   always_ff @(posedge clk or negedge rstn) begin
-    if (~rstn) state <= STATE_IDLE;
-    else state <= nxt_state;
-  end
+    if (~rstn) curr_state <= IDLE;
+    else curr_state <= next_state;
+  end  // State (S)
 
   always_comb begin
-    case (state)
-      STATE_IDLE:
-      nxt_state = (slave.AWVALID) ? STATE_WRITE : (slave.ARVALID) ? STATE_READ : STATE_IDLE;
-      STATE_READ: nxt_state = (slave.RLAST & RFin) ? STATE_IDLE : STATE_READ;
-      STATE_WRITE: nxt_state = (slave.WLAST & BFin) ? STATE_IDLE : STATE_WRITE;
-      default: nxt_state = STATE_IDLE;
+    case (curr_state)
+      IDLE:
+      next_state = (slave.AWVALID) ? WRITE : (slave.ARVALID) ? READ : IDLE;
+      READ: next_state = (slave.RLAST & Rx_hs_done) ? IDLE : READ;
+      WRITE: next_state = (Bx_hs_done) ? IDLE : WRITE;
+      default: next_state = IDLE;
     endcase
-  end
+  end  // Next state (C)
 
   always_comb begin
     slave.ARREADY = 1'b0;
@@ -68,15 +61,16 @@ module sensor_wrapper (
     slave.WREADY  = 1'b0;
     slave.RVALID  = 1'b0;
     slave.BVALID  = 1'b0;
-    case (state)
-      STATE_IDLE: begin
+
+    case (curr_state)
+      IDLE: begin
         slave.ARREADY = ~slave.AWVALID;
         slave.AWREADY = 1'b1;
       end
-      STATE_READ: begin
+      READ: begin
         slave.RVALID = 1'b1;
       end
-      STATE_WRITE: begin
+      WRITE: begin
         slave.BVALID = lockW;
         slave.WREADY = lockB | lockAW;
       end
@@ -85,44 +79,51 @@ module sensor_wrapper (
 
   always_ff @(posedge clk or negedge rstn) begin
     if (~rstn) begin
-      prevID    <= `AXI_IDS_BITS'b0;
-      prevLEN   <= `AXI_LEN_BITS'b0;
-      prevSIZE  <= `AXI_SIZE_BITS'b0;
-      prevADDR  <= `AXI_ADDR_BITS'b0;
-      prevBURST <= 2'b0;
-      cnt       <= `AXI_LEN_BITS'b0;
-      lockAW    <= 1'b0;
-      lockB     <= 1'b0;
-      lockW     <= 1'b0;
+      ID_r    <= `AXI_IDS_BITS'b0;
+      LEN_r   <= `AXI_LEN_BITS'b0;
+      SIZE_r  <= `AXI_SIZE_BITS'b0;
+      ADDR_r  <= `AXI_ADDR_BITS'b0;
+      BURST_r <= 2'b0;
     end else begin
-      lockAW <= (lockAW & WFin) ? 1'b0 : (AWFin) ? 1'b1 : lockAW;
-      lockW <= (WFin) ? 1'b1 : (lockW & BFin) ? 1'b0 : lockW;
-      lockB <= (BFin) ? 1'b1 : (lockB & ~BFin) ? 1'b0 : lockB;
-      prevID <= (AWFin) ? slave.AWID : (ARFin) ? slave.ARID : prevID;
-      prevLEN <= (AWFin) ? slave.AWLEN : (ARFin) ? slave.ARLEN : prevLEN;
-      prevSIZE <= (AWFin) ? slave.AWSIZE : (ARFin) ? slave.ARSIZE : prevSIZE;
-      prevADDR <= (AWFin) ? slave.AWADDR : (ARFin) ? slave.ARADDR : prevADDR;
-      prevBURST   <= (AWFin) ?slave.AWBURST:(ARFin)?slave.ARBURST: prevBURST;
-      cnt         <= ((slave.WLAST & BFin) | (slave.RLAST & RFin)) ? `AXI_LEN_BITS'b0 : (WFin | RFin) ? cnt + `AXI_LEN_BITS'b1 : cnt;
+      ID_r <= (AWx_hs_done) ? slave.AWID : (ARx_hs_done) ? slave.ARID : ID_r;
+      LEN_r <= (AWx_hs_done) ? slave.AWLEN : (ARx_hs_done) ? slave.ARLEN : LEN_r;
+      SIZE_r <= (AWx_hs_done) ? slave.AWSIZE : (ARx_hs_done) ? slave.ARSIZE : SIZE_r;
+      ADDR_r <= (AWx_hs_done) ? slave.AWADDR : (ARx_hs_done) ? slave.ARADDR : ADDR_r;
+      BURST_r   <= (AWx_hs_done) ? slave.AWBURST: (ARx_hs_done)? slave.ARBURST: BURST_r;
     end
-  end
-
-  always_comb begin
-    unique if (prevBURST == `AXI_BURST_FIXED) begin
-      sensorADDR = prevADDR[2+:10];
-    end else if (prevBURST == `AXI_BURST_INC) begin
-      sensorADDR = prevADDR[2+:10] + {2'b0, cnt};
-    end else sensorADDR = prevADDR[2+:10];
   end
 
   always_ff @(posedge clk or negedge rstn) begin
     if (~rstn) begin
-      sctrl_en <= 1'b0;
+      cnt <= `AXI_LEN_BITS'b0;
+      lockAW <= 1'b0;
+      lockB <= 1'b0;
+      lockW <= 1'b0;
+    end else begin
+      cnt <= ((Bx_hs_done) | (slave.RLAST & Rx_hs_done)) ? `AXI_LEN_BITS'b0 : (Wx_hs_done | Rx_hs_done) ? cnt + `AXI_LEN_BITS'b1 : cnt;
+      lockAW <= (lockAW & Wx_hs_done) ? 1'b0 : (AWx_hs_done) ? 1'b1 : lockAW;
+      lockW <= (Wx_hs_done) ? 1'b1 : (lockW & Bx_hs_done) ? 1'b0 : lockW;
+      lockB <= (Bx_hs_done) ? 1'b1 : (lockB & ~Bx_hs_done) ? 1'b0 : lockB;
+    end
+  end
+
+  // sensor control
+  always_ff @(posedge clk or negedge rstn) begin
+    if (~rstn) begin
+      sctrl_en <= SCTRL_DIS;
       sctrl_clear <= 1'b0;
     end else if (slave.WVALID) begin
-      unique if (sensorADDR == `EN_ADDR) sctrl_en <= slave.WDATA[0];
-      else if (sensorADDR == `CLEAR_ADDR) sctrl_clear <= slave.WDATA[0];
+      unique if (sensor_addr == SCTRL_ENB_ADDR) sctrl_en <= slave.WDATA[0];
+      else if (sensor_addr == SCTRL_CLEAR_ADDR) sctrl_clear <= slave.WDATA[0];
     end
+  end
+
+  always_comb begin
+    unique if (BURST_r == `AXI_BURST_FIXED) begin
+      sensor_addr = ADDR_r[2+:10];
+    end else if (BURST_r == `AXI_BURST_INC) begin
+      sensor_addr = ADDR_r[2+:10] + {2'b0, cnt};
+    end else sensor_addr = ADDR_r[2+:10];
   end
 
   sensor_ctrl sensor_ctrl (
@@ -130,7 +131,7 @@ module sensor_wrapper (
       .rst(~rstn),
       .sctrl_en(sctrl_en),
       .sctrl_clear(sctrl_clear),
-      .sctrl_addr(sensorADDR[5:0]),
+      .sctrl_addr(sensor_addr[5:0]),
       .sensor_ready(sensor_ready),
       .sensor_out(sensor_out),
       .sctrl_interrupt(sensor_interrupt),
